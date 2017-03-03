276f2e2611ff2054cdbc34c3213ec4142b8fc355
Nicolas Machado <nicoma63@gmail.com>
2017-03-03 12:59:42 +0100

    Polish

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const morgan = require('morgan');
 const app = express();
 
 const postsRouter = require('./postsRouter');
- 
+
 // log the http layer
 app.use(morgan('common'));
 
 app.use('/posts', postsRouter);
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send('Something went wrong').end();
+    console.log(err);
+    res.status(500).send('Something went wrong').end();
 }); 
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
--- postsRouter.js
+++ postsRouter.js
@@ -12,49 +12,49 @@ BlogPosts.create("This is my second blog entry!", "This is my first blog API!", 
 
 // return all existing entries
 router.get("/", (req, res, next) => {
-  res.send(BlogPosts.get());
+    res.send(BlogPosts.get());
 });
 
 // return existing specific entry
 router.get("/:id", (req, res, next) => {
-  res.send(BlogPosts.get(req.params.id)); 
+    res.send(BlogPosts.get(req.params.id)); 
 });
 
 // post new entry
 router.post("/", jsonParser, (req, res, next) => {
-  if (req.body.title && req.body.content && req.body.author) {
-    BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-    res.send(`Entry called "${req.body.title}" created!`);
-  } else {
-    console.log("A parameter is missing. Cannot CREATE.");
-    next();
-  }
+    if (req.body.title && req.body.content && req.body.author) {
+        BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+        res.send(`Entry called "${req.body.title}" created!`);
+    } else {
+        console.log("A parameter is missing and I'm too lazy to tell you which one. Cannot CREATE.");
+        next();
+    }
 });
 
 // delete entry
 router.delete("/:id", (req, res, next) => {
-  BlogPosts.delete(req.params.id);
-  res.send(`Entry with ID "${req.params.id}" deleted!`);
+    BlogPosts.delete(req.params.id);
+    res.send(`Entry with ID "${req.params.id}" deleted!`);
 });
 
 // edit entry
 router.put("/:id", jsonParser, (req, res, next) => {
-  if (req.params.id && req.body.title && req.body.content && req.body.author) {
-    const updatedEntry = {
-      id: req.params.id, 
-      title: req.body.title, 
-      content: req.body.content, 
-      author: req.body.author
-    };
-    if (req.body.publishDate) {
-      updatedEntry.publishDate = req.body.publishDate;
+    if (req.params.id && req.body.title && req.body.content && req.body.author) {
+        const updatedEntry = {
+            id: req.params.id, 
+            title: req.body.title, 
+            content: req.body.content, 
+            author: req.body.author
+        };
+        if (req.body.publishDate) {
+            updatedEntry.publishDate = req.body.publishDate;
+        }
+        BlogPosts.update(updatedEntry);
+        res.send(`Entry with ID "${req.params.id}" updated!`);
+    } else {
+        console.log("A parameter is missing and I'm too lazy to tell you which one. Cannot EDIT.");
+        next();   
     }
-    BlogPosts.update(updatedEntry);
-    res.send(`Entry with title "${updatedEntry.title}" updated!`);
-  } else {
-    console.log("A parameter is missing. Cannot EDIT.");
-    next();   
-  }
 });
 
 module.exports = router;
