7617b8f26f15760424d27a50d78d9a05e4d7a542
Nicolas Machado <nicoma63@gmail.com>
2017-03-03 12:43:54 +0100

    Added router

---
+++ postsRouter.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// create default entries
+BlogPosts.create("This is my first blog entry!", "And the sun might or might not be shining!", "Nicolas");
+BlogPosts.create("This is my second blog entry!", "This is my first blog API!", "John");
+
+// return all existing entries
+router.get("/", (req, res, next) => {
+  res.send(BlogPosts.get());
+});
+
+// return existing specific entry
+router.get("/:id", (req, res, next) => {
+  res.send(BlogPosts.get(req.params.id)); 
+});
+
+// post new entry
+router.post("/", jsonParser, (req, res, next) => {
+  if (req.body.title && req.body.content && req.body.author) {
+    BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.send(`Entry called "${req.body.title}" created!`);
+  } else {
+    console.log("A parameter is missing. Cannot CREATE.");
+    next();
+  }
+});
+
+// delete entry
+router.delete("/:id", (req, res, next) => {
+  BlogPosts.delete(req.params.id);
+  res.send("Entry deleted!");
+});
+
+// edit entry
+router.put("/:id", jsonParser, (req, res, next) => {
+  if (req.params.id && req.body.title && req.body.content && req.body.author) {
+    const updatedEntry = {
+      id: req.params.id, 
+      title: req.body.title, 
+      content: req.body.content, 
+      author: req.body.author
+    };
+    if (req.body.publishDate) {
+      updatedEntry.publishDate = req.body.publishDate;
+    }
+    BlogPosts.update(updatedEntry);
+    res.send(`Entry ${req.params.id} updated!`);
+  } else {
+    console.log("A parameter is missing. Cannot EDIT.");
+    next();   
+  }
+});
+
+module.exports = router;
